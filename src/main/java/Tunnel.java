3514a65b152cd3cbd15582ea853fe2ba38db9d9a
AlekseyBog <bogdanov.lexa1234@yandex.ru>
2022-04-18 14:42:15 +0300

    1

--- src/main/java/Tunnel.java
+++ src/main/java/Tunnel.java
@@ -3,22 +3,19 @@ public class Tunnel extends Stage {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
     }
+
     @Override
     public void go(Car c) {
         try {
-            try {
-                System.out.println(c.getName() + " готовится к этапу(ждет): " +
-                        description);
-                System.out.println(c.getName() + " начал этап: " + description);
-                Thread.sleep(length / c.getSpeed() * 1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                System.out.println(c.getName() + " закончил этап: " +
-                        description);
-            }
-        } catch (Exception e) {
+            System.out.println(c.getName() + " готовится к этапу(ждет): " +
+                    description);
+            System.out.println(c.getName() + " начал этап: " + description);
+            Thread.sleep(length / c.getSpeed() * 1000);
+        } catch (InterruptedException e) {
             e.printStackTrace();
+        } finally {
+            System.out.println(c.getName() + " закончил этап: " +
+                    description);
         }
     }
 }
